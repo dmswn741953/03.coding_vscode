853817d104cf8f20ecc574688169b20e9b84f78e
Your Name <you@example.com>
2025-05-04 23:08:51 +0900

    first commit

--- ★포토폴리오/mallhankook/js/250504_mallhankook.js
+++ ★포토폴리오/mallhankook/js/250504_mallhankook.js
@@ -20,14 +20,41 @@ var Main_Swiper = new Swiper(".Main_Swiper", {
       document.querySelectorAll(".menu li").forEach(function (v) {
         v.querySelector("a").classList.remove("on");
       });
+      document
+        .querySelectorAll(".service li")[0]
+        .querySelectorAll("a")
+        .forEach((v) => {
+          v.classList.remove("on");
+        });
+      document
+        .querySelectorAll(".service li")[1]
+        .querySelector("a")
+        .classList.remove("on");
       document.querySelector(".nav_logo img").src = "./images/logo_black.png";
+
+      document.querySelectorAll(".nav_icon").forEach((v) => {
+        v.classList.remove("on");
+      });
       // 현재 활성화된 슬라이드의 실제 인덱스를 가져옴
       var realIndex = this.realIndex;
 
       // 조건에 맞는 인덱스에서 메뉴 항목에 .on 클래스 추가
       if (realIndex === 0) {
-        document.querySelectorAll(".menu li").forEach(function (v) {
+        document.querySelectorAll(".menu li").forEach((v) => {
           v.querySelector("a").classList.add("on");
+        });
+        document
+          .querySelectorAll(".service li")[0]
+          .querySelectorAll("a")
+          .forEach((v) => {
+            v.classList.add("on");
+          });
+        document
+          .querySelectorAll(".service li")[1]
+          .querySelector("a")
+          .classList.add("on");
+        document.querySelectorAll(".nav_icon").forEach((v) => {
+          v.classList.add("on");
         });
       } else if (realIndex === 1) {
         document.querySelector(".nav_logo img").src = "./images/logo_white.png";
@@ -37,15 +64,54 @@ var Main_Swiper = new Swiper(".Main_Swiper", {
         document.querySelectorAll(".menu li").forEach(function (v) {
           v.querySelector("a").classList.add("on");
         });
+        document
+          .querySelectorAll(".service li")[0]
+          .querySelectorAll("a")
+          .forEach((v) => {
+            v.classList.add("on");
+          });
+        document
+          .querySelectorAll(".service li")[1]
+          .querySelector("a")
+          .classList.add("on");
+        document.querySelectorAll(".nav_icon").forEach((v) => {
+          v.classList.add("on");
+        });
       } else if (realIndex === 4) {
         document.querySelector(".nav_logo img").src = "./images/logo_white.png";
       } else if (realIndex === 5) {
         document.querySelectorAll(".menu li").forEach(function (v) {
           v.querySelector("a").classList.add("on");
         });
+        document
+          .querySelectorAll(".service li")[0]
+          .querySelectorAll("a")
+          .forEach((v) => {
+            v.classList.add("on");
+          });
+        document
+          .querySelectorAll(".service li")[1]
+          .querySelector("a")
+          .classList.add("on");
+        document.querySelectorAll(".nav_icon").forEach((v) => {
+          v.classList.add("on");
+        });
       } else if (realIndex === 6) {
         document.querySelectorAll(".menu li").forEach(function (v) {
           v.querySelector("a").classList.add("on");
+          document
+            .querySelectorAll(".service li")[0]
+            .querySelectorAll("a")
+            .forEach((v) => {
+              v.classList.add("on");
+            });
+          document
+            .querySelectorAll(".service li")[1]
+            .querySelector("a")
+            .classList.add("on");
+          document.querySelectorAll(".nav_icon").forEach((v) => {
+            v.classList.add("on");
+          });
         });
       } else if (realIndex === 7) {
         document.querySelector(".nav_logo img").src = "./images/logo_white.png";
