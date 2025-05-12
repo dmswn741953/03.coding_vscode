feeb689f5864bbac471b8f4f2498c634f90d3b32
dmswn741953 <dmswn741953@naver.com>
2025-05-12 14:53:08 +0900

    0512 commit

--- ★포토폴리오/rendingPage/js/index.js
+++ ★포토폴리오/rendingPage/js/index.js
@@ -1,4 +1,4 @@
-const cloudWrap = document.querySelector(".cloud-wrap");
+/*const cloudWrap = document.querySelector(".cloud-wrap");
 const main = document.querySelector(".main");
 
 cloudWrap.addEventListener("click", () => {
@@ -7,4 +7,12 @@ cloudWrap.addEventListener("click", () => {
   setTimeout(() => {
     main.classList.add("active");
   }, 1000); // 구름 확대 & 텍스트 fade-out 후 표시
+});*/
+$("body").css({ overflow: "hidden" });
+
+$(".main").css({ opacity: "0" });
+$(".cloud-wrap img").click(function () {
+  $("body").css({ overflow: "visible" });
+  $(".main").css({ opacity: "1", zIndex: 11 });
+  $(".wrap").css({ opacity: "0", visibility: "hidden" });
 });
